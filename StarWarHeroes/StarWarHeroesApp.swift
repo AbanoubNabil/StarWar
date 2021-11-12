32fab8f7635123d2beedf13e140f337c61c332ec
apple <Abanoub.Habib@warbabank.com>
2021-11-12 21:56:35 +0200

    Initial Commit

---
+++ StarWarHeroes/StarWarHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarHeroesApp.swift
+//  StarWarHeroes
+//
+//  Created by Abanoub Nabil on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StarWarHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
